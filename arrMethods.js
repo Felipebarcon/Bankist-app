f29d094586b90ff4099d71874c7a9d23cf723892
Felipe Barcon <felipe.barcon@gmail.com>
2022-09-06 15:12:05 +0200

    adding account movements to movement container

--- arrMethods.js
+++ arrMethods.js
@@ -1,3 +1,15 @@
+// LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling']
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 // SLICE ==> return a new array but don't change original array
