4df577652541508d2374fce2fceed6b687745549
Felipe Barcon <felipe.barcon@gmail.com>
2022-09-12 10:50:47 +0200

    dates

--- challenge.js
+++ challenge.js
@@ -700,4 +700,40 @@ console.log(huge + " Is REALLY big!!!");
 
 // Division
 console.log(10n / 3n);
+
+
+console.log("---------------Dates and Time---------------");
+
+// Create a date
+const now = new Date();
+console.log(now);
+
+console.log(new Date("Mon Sep 12 2022 10:37:12"));
+console.log(new Date("December 24, 2022"));
+
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 25, 50));
+console.log(new Date(2037, 10, 31)); // autocorrect date
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
 */
+// Working with Dates
+const future = new Date(2037, 10, 19, 15, 25);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142253500000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
