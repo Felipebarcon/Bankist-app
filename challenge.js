4c7395f587dbf247e8c20f3cff07a7763bcc5536
Felipe Barcon <felipe.barcon@gmail.com>
2022-09-12 12:42:17 +0200

    inplementing logic with dates on transfers and loans

--- challenge.js
+++ challenge.js
@@ -738,3 +738,12 @@ console.log(Date.now());
 future.setFullYear(2040);
 console.log(future);
 */
+
+const future = new Date(2037, 10, 19, 15, 25);
+console.log(Number(future));
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 19), new Date(2037, 3, 9));
+console.log(days1);
